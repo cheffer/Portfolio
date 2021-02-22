fa6ad3bb76a948ac95cd771d8b2af90ff218f71c
Cheffer <bruno.cb.32@gmail.com>
2021-02-22 20:07:32 -0300

    add pages file

---
+++ MeuPortfolio/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<!-- Author -->
+	<link rel="Bruno Cheffer Belinassi" href="https://www.linkedin.com/in/bruno-cheffer-belinassi-2a228165/"/>
+
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+     <!-- FontAwseen CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"> <!--load all styles -->
+
+    <!-- HTML5Shiv -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <![endif]-->
+
+    <!-- Customized Style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- script -->
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="js/script.js"></script>
+
+	<title>Bruno Cheffer Belinassi</title>
+
+</head>
+<body>
+	<header> <!-- start header -->
+
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-transparente"> <!-- start nav -->
+			<div class="container d-flex"> <!-- start container -->
+				<a href="index.php" class="navbar-brand">
+	    		Home
+	  		</a>  
+	  		<span class="navbar-brand d-none d-md-block">|</span>
+	  		<a href="#" id="idPagJogos" class="navbar-brand">
+	    		Jogos
+	  		</a> 
+	  		<span class="navbar-brand d-none d-md-block">|</span>
+	  		<a href="#" id="idPagProjetos" class="navbar-brand">
+	    		Projetos
+	  		</a>  
+
+	  		<button class="navbar-toggler" data-toggle="collapse" data-target="#nav-main">
+	  			<i class="fas fa-bars text-white"></i>          			
+	  		</button>
+
+	  		<div class="collapse navbar-collapse" id="nav-main">
+	  			<ul class="nav navbar-nav ml-auto">
+	  				<li class="nav-item">
+	  					<a href="https://pt-br.facebook.com/bcheffer" target="_blank" class="nav-link">
+	  						<i class="fab fa-facebook text-primary"></i>
+	  					</a>
+	  				</li>      			
+	  			
+	  				<li class="nav-item">
+	  					<a href="https://br.linkedin.com/in/bruno-cheffer-belinassi-2a228165" target="_blank" class="nav-link">
+	  						<i class="fab fa-linkedin text-primary"></i>
+	  					</a>
+	  				</li>      			
+	  			
+	  				<li class="nav-item">
+	  					<a href="https://github.com/cheffer" target="_blank" class="nav-link">
+	  						<i class="fab fa-github"></i>
+	  					</a>
+	  				</li>
+	  			</ul>
+	  		</div>
+			</div>	 <!-- end container -->		
+		</nav> <!-- end nav -->		
+	</header> <!-- end header -->
+
+<div class="menu_top">
+  <div class="container"> <!-- start container top -->   
+      <div>
+        <img src="img/avatar.jpg" class="rounded-circle shadow p-1" width="120">              
+      </div>
+      <div>
+        <h2>Bruno Cheffer Belinassi</h2>
+        <p>Site com trabalhos em que participei no desenvolvimento</p>
+      </div>              
+    </div> <!-- end container top --> 
+</div> <!-- end div menu-top -->  
+	
+
+	<section id="home"> <!-- start section home -->	
+		<div class="container section-container">
+			<div class="row">
+				<div class="col-sm-11 col-md-8">
+					<div id="pagina">
+						<div class="card marker">
+					      	<div class="card-header">
+					        	Em destaque
+					      	</div>
+					      	<div class="card-body">
+					      		<div id="portfolio-carousel" class="carousel slide" data-ride="carousel">
+				                  <ol class="carousel-indicators">
+				                    <li data-target="#portfolio-carousel" data-slide-to="0" class="active"></li>
+				                    <li data-target="#portfolio-carousel" data-slide-to="1"></li>
+				                    <li data-target="#portfolio-carousel" data-slide-to="2"></li>
+				                  </ol>
+				                  <div class="carousel-inner">
+				                    <div class="carousel-item active">
+				                      <img src="img/escape-galaxy/img-escape-galaxy-001.png" class="d-block destaque-img" alt="Primeira imagem"> 
+				                    </div>
+				                    <div class="carousel-item">
+				                      <img src="img/escape-galaxy/img-escape-galaxy-002.png" class="d-block destaque-img" alt="Segunda imagem"> 
+				                    </div>
+				                    <div class="carousel-item">
+				                      <img src="img/escape-galaxy/img-escape-galaxy-003.png" class="d-block destaque-img" alt="Terceira imagem"> 
+				                    </div>
+				                  </div>
+				                  <a class="carousel-control-prev" href="#portfolio-carousel" role="button" data-slide="prev">
+				                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				                    <span class="sr-only">Anterior</span>
+				                  </a>
+				                  <a class="carousel-control-next" href="#portfolio-carousel" role="button" data-slide="next">
+				                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				                    <span class="sr-only">Próximo</span>
+				                  </a>                  
+	                			</div>
+	                			
+						        <h4 class="card-title">Escape in the Galaxy</h4>
+					        	<h6 class="card-subtitle">Em pausa | PC | Não publicado</h6>
+					        	<p class="card-text">No jogo seu personagem é um robo que precisa escapar de uma nave encontrando a saída em cada sala que esteja.</p>
+					        	<a class="btn btn-warning" href="">Leia mais</a>
+					      	</div>
+					    </div>	
+					</div>
+				</div>
+				<div class="col-sm-11 col-md-3 perfil">
+					<img src="img/bruno-foto.png" class="rounded shadow p-2" width="120"> 
+					<hr>
+					<h5><u>Bruno Cheffer Belinassi</u></h5>
+					<div id="graduacoes"></div>
+					<hr>
+					<h5><u>Links uteis</u></h5>
+					<div id="link_cv"></div>
+					<hr>
+				</div>
+			</div>			
+		</div>	
+	</section> <!-- end section home -->	
+    <footer>
+    	<div class="container">
+    		<div class="row">     					
+
+	    		<div class="col-md-6">
+	    			<h1 class="border-bottom">Contato</h1>	
+
+						<p>Entre em contato comigo.</p>
+						<form class="form" action="./enviar_email.php" method="post">
+
+						<div class="form-group form-group-contato row">
+							<div class="col-sm-10"> 
+								<label for="name">Nome (Obrigatório) </label>
+        						<input name="name" type="text" class="form-control form-control-sm mr-2"  id="name" placeholder="Digite seu nome">
+        					</div>
+
+        					<div class="col-sm-10">
+        						<label for="email">E-mail (Obrigatório)</label>
+            					<input name="email" type="email" class="form-control" id="email" placeholder="Digite seu melhor e-mail">
+            					<!--<div class="valid-feedback">E-mail Válido</div>-->
+            				</div>
+
+            				<div class="col-sm-10">
+            					<label for="site">Site</label>
+            					<input name="site" type="text" class="form-control" id="site" placeholder="Digite seu site">
+            				</div>
+
+            				<div class="col-sm-10">
+            					<label for="mensagem">Mensagem (Obrigatório)</label>
+            					<textarea name="message" class="form-control" id="mensagem" rows="5" ></textarea>
+            				</div>
+            				<div class="col-sm-10 pt-2">
+            					                					
+                					<button type="submit" id="idBtnEnviar" class="btn btn-success">Enviar</button>
+                					<!--<button class="btn btn-light">Anexar</button>-->
+            						
+            				</div>
+						</div>
+
+					</form>
+						
+	    		</div>
+
+                <div class="col-md-6 row">
+                    <div class="col-sm-12">
+                        <h3>Agradecimentos: </h3>
+                        <p> Tenho a agradecer minha família e amigos por sempre me apoiar e incentivar com meus estudos e projetos!
+                        </p>    
+
+                    </div>
+                </div>     		
+    		</div>
+    		<div class="row">
+                <div class="col border-top border-white pt-2">
+                        <p>&copy; 2020 BRUNO CHEFFER BELINASSI</p>
+                </div>
+            </div>
+    	</div>        
+    </footer>
+
+</body>
+</html>
